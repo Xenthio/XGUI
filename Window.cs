d39ccf4ff9c0b321fb31199f295abe035db76e41
Xenthio <thegooster88@gmail.com>
2023-01-10 15:40:40 +1100

    Base Window Class

---
+++ Window.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox.UI;
+
+namespace XGUI;
+public class Window : Panel
+{
+	public Vector2 WindowPosition;
+	public int WindowZIndex;
+
+	public string WindowTitle;
+	public bool WindowControls;
+	public bool WindowResizable;
+}
