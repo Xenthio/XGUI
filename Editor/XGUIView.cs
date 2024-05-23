f8a84dafd1e93fbdb4a4a9c6b9fc030a4d2d184b
Xenthio <thegooster88@gmail.com>
2024-05-23 16:38:11 +1000

    add a model so i can tell if rendering is working

--- Editor/XGUIView.cs
+++ Editor/XGUIView.cs
@@ -8,6 +8,7 @@ public class XGUIView : NativeRenderingWidget
 {
 	RootPanel rootpanel;
 	XGUIRootPanel panel;
+	SceneModel model;
 
 	SceneWorld world;
 
@@ -18,9 +19,10 @@ public class XGUIView : NativeRenderingWidget
 		MinimumSize = 300;
 
 		world = new SceneWorld();
+		model = new SceneModel( world, "models/dev/gordon_at_desk.vmdl", Transform.Zero );
 		Camera = new SceneCamera( "XGUIRenderView" );
 		Camera.World = world;
-		Camera.BackgroundColor = Color.Yellow;
+		Camera.BackgroundColor = Color.Black;
 
 		rootpanel = new RootPanel();
 		rootpanel.Scene = Scene;
@@ -31,13 +33,27 @@ public class XGUIView : NativeRenderingWidget
 		Window.StyleSheet.Load( "/XGUI/DefaultStyles/OliveGreen.scss" );
 		panel.AddChild( Window );
 
+
+		new SceneLight( world, new Vector3( 100, 100, 100 ), 500, Color.Orange * 3 );
+		new SceneLight( world, new Vector3( -100, -100, 100 ), 500, Color.Cyan * 3 );
+
 		Window.Size = new Vector2( 800, 600 );
 
 	}
+	Vector2 lastPos;
+	float spinVelocity;
+	float hoverTime;
 	public override void PreFrame()
 	{
+		var dir = new Vector3( 0.5f, 0.5f, 0.5f ).Normal;
 
-		Camera.World.AmbientLightColor = IsUnderMouse ? Color.Gray : Color.Black;
+		var distance = MathX.SphereCameraDistance( model.Bounds.Size.Length * 0.5f, Camera.FieldOfView );
+		var aspect = Size.x / Size.y;
+		if ( aspect > 1 ) distance *= aspect;
+
+		Camera.World.AmbientLightColor = Color.Gray;
+		Camera.Position = model.Bounds.Center + dir * distance * 0.9f;
+		Camera.Rotation = Rotation.LookAt( dir * -1.0f, Vector3.Up );
 		Camera.ZFar = 10000;
 		Camera.FieldOfView = 50;
 		Camera.AntiAliasing = false;
